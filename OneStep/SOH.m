d0bebc5b474bbfc9e13217d0ce976d6dc348de84
Hutianzhong <48516724+Hutianzhong@users.noreply.github.com>
2019-08-19 11:44:16 +0800

    Add files via upload

---
+++ OneStep/SOH.m
@@ -0,0 +1,18 @@
+function [s,c] = SOH(var)
+
+var = var.B0005.cycle;
+c = 0;
+s = [];
+
+for i = 1 : size(var,2)
+    type = var(i).type;
+    if type == "discharge"
+        c = c + 1;
+        cap = var(i).data.Capacity;
+        s = [s;cap];
+    end
+end
+    save('data.xlcx','s');
+    s = s/s(1);
+end
+
